8619a2aad108cfefb77017448ac0d3b586779e60
Michael Dart <mdart86@hotmail.com>
2021-07-29 15:13:02 +1000

    post router completed

---
+++ routes/postRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+const {getPosts, newPost, getPost, removePost, changePost} = require('../controllers/postsController')
+const {loginRequired} = require('../controllers/authController')
+
+router.use(loginRequired)
+
+router.get('/', getPosts)
+router.get('/:id', getPost)
+
+
+
+router.post('/', newPost)
+
+router.delete('/:id', removePost)
+
+router.put('/:id', changePost)
+
+module.exports = router
