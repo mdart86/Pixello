3fcdf2fc9c0816344325ba7f1f788a167b016cf6
emigraced <helloemilymills@gmail.com>
2021-08-01 15:16:33 +1000

    added drop down menu for categories

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -27,6 +27,12 @@ export const CreatePost = () => {
                     </Label>
                     <Input type="file" id="photo-upload" accept=".png, .jpg, .jpeg" hidden onChange={displayFileInfo}/>
                     <TextArea id="caption" placeholder="Give it a caption"/>
+                    <Select name="category">
+                        <option value="default" hidden>Categories</option>
+                        <option value="film">Film</option>
+                        <option value="candid">Candid</option>
+                        <option value="water">Water</option>
+                    </Select>
                     <Input share="true" type="submit" value="Share!"/>
                 </Form>
             </Container>
@@ -129,6 +135,14 @@ const TextArea = styled.textarea`
         outline: none;
         border: 3px solid var(--dark-pink);
     }
+`
+
+const Select = styled.select`
+    width: 50%;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    background: var(--white);
+    border: 3px solid var(--blue);
 `
 
 //for the share button, as the other input is hidden
