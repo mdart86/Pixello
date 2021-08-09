a969353f341bb2a55d23a84906f880b5896ecfc4
emigraced <helloemilymills@gmail.com>
2021-08-09 21:50:52 +1000

    receiving post data from the API. working on integrating this with the posts component. also added message to home page for if there are no posts

--- src/components/styled/CenteringContainer.styled.js
+++ src/components/styled/CenteringContainer.styled.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 //messages
 //not found
+//home
 export const CenteringContainer = styled.div`
     display: flex;
     justify-content: center; 
-    ${props => props.notfound || props.pleasesignin? "flex-direction: column; text-align: center; margin-top: 50px;" : null}
+    ${props => props.notfound || props.pleasesignin || props.nopostsyet ? "flex-direction: column; text-align: center; margin-top: 50px;" : null}
 `
