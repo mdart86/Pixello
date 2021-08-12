8acd1e49b8eec1dae4c9a1aeafd9a7f7997c7a66
Michael <mdart86@hotmail.com>
2021-08-13 08:42:16 +1000

    updates to comments utils

--- utils/commentUtils.js
+++ utils/commentUtils.js
@@ -3,13 +3,14 @@ const Comment = require ('../models/comment')
 
 // Helper function to find comment by id
 const getCommentById = function () {
-    // Comment.find({postId: {$all : [post._id]}})
-    // Comment.find({})
-    //         .where(postId).equals(postId)
-    // Comment.query.select(postId)
-    const Comment = query.Comment.$where(function () {
-        return postId === postId
-    })
+    return Comment.find()
+    // // Comment.find({postId: {$all : [post._id]}})
+    // // Comment.find({})
+    // //         .where(postId).equals(postId)
+    // // Comment.query.select(postId)
+    // const Comment = query.Comment.$where(function () {
+    //     return postId === postId
+    // })
 }
 
 // Helper function to find comment by id and remove
